009756a165e2b96c37a00f9defb0e616fabe64de
Mikhail Rantovich <mrantovich@gmail.com>
2021-06-08 12:56:17 +0700

    Вёрстка футера

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 const burger = document.querySelector('.burger');
 const headerBox = document.querySelector('.header__box');
+const wholeHTML = document.querySelector('html');
+const wholeBody = document.querySelector('body');
 burger.addEventListener('click', toggleActive);
 
 function toggleActive() {
 	burger.classList.toggle('_active');
 	headerBox.classList.toggle('_active');
+	wholeHTML.classList.toggle('_lock');
+	wholeBody.classList.toggle('_lock');
 }
